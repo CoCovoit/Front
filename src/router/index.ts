b0af0141c79b35f52aa2af8a899f97aab2b9be97
Matteo-Nossro <matnoss@gmail.com>
2025-03-31 02:08:01 +0200

    Ajout de la configuration TypeScript, intégration de PrimeVue et Pinia, mise à jour des dépendances, et refonte de la structure de l'application avec Vue Router. Suppression de main.js au profit de main.ts. Ajout de styles Tailwind CSS.

---
+++ src/router/index.ts
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
+import App from '../App.vue'
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'home',
+    component: App
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
+})
+
+export default router 
