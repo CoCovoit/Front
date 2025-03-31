1c42c824767ead9e6803d47ff342813a7c8ffa8e
Matteo-Nossro <matnoss@gmail.com>
2025-03-31 23:14:36 +0200

    Ajout du composant DefaultContainer pour les card, mise à jour de la page d'accueil pour intégrer ce nouveau composant, et passage de l'objet utilisateur dans le composant Header. Amélioration des styles dans MainLayout.

--- src/Pages/Home.vue
+++ src/Pages/Home.vue
@@ -2,11 +2,17 @@
     <div>
         <h1>Home</h1>
         <MainButton icon="pi pi-plus" label="Créer un trajet" />
+        <DefaultContainer title="Mes trajets">
+            <div class="content">
+            <p>Vous n'avez pas de trajets</p>
+            </div>
+        </DefaultContainer>
     </div>
 </template>
 
 <script setup lang="ts">
 import MainButton from '../components/MainButton.vue';
+import DefaultContainer from '../components/DefaultContainer.vue';
 </script>
 
 <style scoped lang="scss">
