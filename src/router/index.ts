fea38853e3c9f20f6c7a3e1395e5a083c454ad56
Matteo-Nossro <mnossereau@lmhabitat.fr>
2025-06-11 14:09:47 +0200

    feat : Correction login dans routeur

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,42 +1,43 @@
-import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
+import {createRouter, createWebHistory, type RouteRecordRaw} from 'vue-router'
 import MainLayout from '../components/MainLayout.vue'
 import Dashboard from '../Pages/Dashboard.vue'
 import Login from '../Pages/Login.vue'
 import SearchTrip from "@/Pages/SearchTrip.vue";
 import KeyNumber from "@/Pages/KeyNumber.vue";
+
 const routes: RouteRecordRaw[] = [
-  {
-    path: '/',
-    component: MainLayout,
-    children: [
-      {
-        path: '',
-        name: 'dashboard',
-        component: Dashboard
-      },
-      {
+    {
+        path: '/',
+        component: MainLayout,
+        children: [
+            {
+                path: '',
+                name: 'dashboard',
+                component: Dashboard
+            },
+            {
+                path: '/rechercher-un-trajet',
+                name: 'Rechercher un trajet',
+                component: SearchTrip
+            },
+            {
+                path: '/chiffres-cles',
+                name: 'chiffres clés',
+                component: KeyNumber
+            }
+        ]
+    },
+    {
         path: '/login',
         name: 'login',
         component: Login
-      },
-      {
-        path: '/rechercher-un-trajet',
-        name: 'Rechercher un trajet',
-        component: SearchTrip
-      },
-      {
-        path: '/chiffres-cles',
-        name: 'chiffres clés',
-        component: KeyNumber
-      }
-    ]
-  }
-  
+    },
+
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes
 })
 
 export default router 
