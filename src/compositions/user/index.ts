03639ba3780c5280ea5fa3ea4b805b186caf57ef
Matteo-Nossro <matnoss@gmail.com>
2025-06-15 18:24:53 +0200

    feat: implement user management API with create and fetch functions

--- src/compositions/user/index.ts
+++ src/compositions/user/index.ts
@@ -1,3 +1,5 @@
+import {Localisation} from "@/compositions/localisation";
+
 export interface User {
     id: string;
     nom: string;
@@ -13,7 +15,71 @@ export interface User {
     age?: number;
     telephone?: string;
     notificationPush?: boolean;
+    LocalisationId?: Localisation[];
 }
 
+export interface UserRequestDTO {
+    nom?: string;
+    localisationId: number;
+    email?: string;
+}
+
+export interface UserResponseDTO {
+    id: number;
+    nom?: string;
+    email?: string;
+    localisations: Localisation[];
+}
+
+const urlApi = import.meta.env.VITE_API_ENDPOINT;
 
 
+async function createUser(
+    payload: UserRequestDTO
+): Promise<UserResponseDTO> {
+    const urlApi = import.meta.env.VITE_API_ENDPOINT;
+    try {
+        const response = await fetch(`${urlApi}/Utilisateurs`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(payload),
+        });
+
+        if (!response.ok) {
+            throw new Error(`Erreur HTTP ${response.status}`);
+        }
+
+        return (await response.json()) as UserResponseDTO;
+    } catch (error) {
+        console.error('Erreur lors de la création de l\'utilisateur', error);
+        throw error;
+    }
+}
+
+async function getUsers(): Promise<UserResponseDTO[]> {
+    const urlApi = import.meta.env.VITE_API_ENDPOINT;
+    try {
+        const response = await fetch(`${urlApi}/Utilisateurs`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+            },
+        });
+        if (!response.ok) {
+            throw new Error(`Erreur HTTP ${response.status}`);
+        }
+        return (await response.json()) as UserResponseDTO[];
+    } catch (error) {
+        console.error('Erreur lors de la récupération des utilisateurs', error);
+        throw error;
+    }
+}
+
+export function useUser(){
+    return{
+        createUser,
+        getUsers
+    }
+}
